6e788a1834f125bae8475953a86fd5405eebd02d
Sumedh-Raghoji <sumedhraghoji10@gmail.com>
2024-01-10 01:25:34 -0600

    second commit

---
+++ drive.py
@@ -0,0 +1,58 @@
+import numpy as np
+import socketio
+import eventlet
+from flask import Flask
+import tensorflow
+# from keras.models import load_model
+from tensorflow.keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
+from keras.optimizers import Adam
+import os
+
+sio = socketio.Server()
+
+app = Flask(__name__) #'__main__'
+
+speed_limit = 10
+def img_preprocess(img):
+    img = img[60:135,:,:]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img,  (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    speed = float(data['speed'])
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = img_preprocess(image)
+    image = np.array([image])
+    steering_angle = float(model.predict(image))
+    throttle = 1.0 - speed/speed_limit
+    print('{} {} {}'.format(steering_angle, throttle, speed))
+    send_control(steering_angle, throttle)
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('connected')
+    send_control(0, 1)
+
+def send_control(steering_angle, throttle):
+    sio.emit('steer', data= {
+        'steering_angle' : steering_angle.__str__(),
+        'throttle' : throttle.__str__()
+    })
+
+if __name__ == '__main__':
+    # model = load_model('Behclon.h5')
+    file_name = os.path.dirname(__file__) +'\Behclon.h5'
+    # test_dataset = h5py.File(file_name, "r")
+    model = load_model(file_name, compile=False)
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
